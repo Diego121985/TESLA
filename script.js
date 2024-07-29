d323ee1a2a692b8f731d243ab167341741963eb7
Diego121985 <diegoleite59986@gmail.com>
2024-07-29 19:42:41 -0300

    Projeto- TESLA

---
+++ script.js
@@ -0,0 +1,12 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left ="40%"
+
+
+}
+function desaparecerFormulario(){
+  formulario.style.left = "-40%"
+
+
+}
