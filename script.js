fdea60980b827988996e0087c5ae3822be16ee41
Diego121985 <diegoleite59986@gmail.com>
2024-08-03 18:05:38 -0300

    script

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let formulario = document.querySelector(".formulario")
 
 function aparecerFormulario(){
-    formulario.style.left ="40%"
+    formulario.style.left ="35%"
 
 
 }
 function desaparecerFormulario(){
-  formulario.style.left = "-40%"
+  formulario.style.left = "-30%"
 
 
 }
