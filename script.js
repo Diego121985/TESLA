47006c15a4a442b283e9582cca95a165576ee41e
Diego121985 <diegoleite59986@gmail.com>
2024-11-26 21:32:48 -0300

    image

---
+++ script.js
@@ -0,0 +1,12 @@
+let formulario = document.querySelector(".formulario")
+
+function aparecerFormulario(){
+    formulario.style.left ="39%"
+
+
+}
+function desaparecerFormulario(){
+  formulario.style.left = "-100%"
+
+
+}
